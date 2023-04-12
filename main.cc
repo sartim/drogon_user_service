b00b3dfc467df3f34d57cc58304bb67b0289e4a1
Tim <sarrtim@gmail.com>
2023-04-12 21:28:05 +0300

    Add comments and update user route registered

--- main.cc
+++ main.cc
@@ -3,33 +3,42 @@
 
 
 int main() {
+    // Set log level
     drogon::app().setLogLevel(trantor::Logger::kTrace);
+
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0",8000);
+
     //Load config file
     drogon::app().loadConfigFile("../config.json");
-    //Run HTTP framework,the method will block in the internal event loop
+
+    // Register root route
     drogon::app().registerHandler(
-        "/hello", 
+        "/", 
         [](
             const drogon::HttpRequestPtr& req, std::function<void (
                 const drogon::HttpResponsePtr&
                 )>&& callback
           ) {
         drogon::HttpResponsePtr resp = drogon::HttpResponse::newHttpResponse();
-        resp->setBody("Hello, world!");
+        Json::Value response;
+            response["status"] = "up";
+        resp->setBody("Welcome to drogon user service");
         callback(resp);
     });
+
+    // Register user route
     auto userController = std::make_shared<UserController>();
     drogon::app().registerHandler(
-        "/user", [userController](
+        "/api/v1/user", [userController](
             const HttpRequestPtr& req, std::function<void(
                 const HttpResponsePtr&
                 )>&& callback) {
         userController->getUsers(req, std::move(callback));
     }, {Get});
 
-
+    // Run server
     drogon::app().run();
+
     return 0;
 }
