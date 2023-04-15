4a0efb0147aeadb7b16482e1b3afbe38f7dd8206
Tim <sarrtim@gmail.com>
2023-04-15 23:33:05 +0300

    Add auth filter

---
+++ filters/AuthFilter.h
@@ -0,0 +1,20 @@
+/**
+ *
+ *  AuthFilter.h
+ *
+ */
+
+#pragma once
+
+#include <drogon/HttpFilter.h>
+using namespace drogon;
+
+
+class AuthFilter : public HttpFilter<AuthFilter>
+{
+  public:
+    void doFilter(const HttpRequestPtr &req,
+                  FilterCallback &&fcb,
+                  FilterChainCallback &&fccb) override;
+};
+
