c596d468d71d46b4483a812a0695d9e86ec0f167
Tim <sarrtim@gmail.com>
2023-04-16 01:08:31 +0300

    Add user table methods

---
+++ tables/UserTable.cc
@@ -0,0 +1,17 @@
+#include <drogon/drogon.h>
+#include "UserTable.h"
+
+using namespace drogon;
+using namespace drogon::orm;
+
+void UserTable::create() {
+    //
+}
+
+void UserTable::alter() {
+    //
+}
+
+void UserTable::_delete() {
+    //
+}
