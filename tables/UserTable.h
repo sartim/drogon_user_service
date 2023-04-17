d0f9162e3d9c790df892aaa082564a499afe0241
Tim <sarrtim@gmail.com>
2023-04-17 09:43:40 +0300

    Update to take client param & add default behavior

--- tables/UserTable.h
+++ tables/UserTable.h
@@ -8,8 +8,11 @@ using namespace drogon::orm;
 class UserTable
 {
   public:
-    void create();
-    void alter();
-    void _delete();
+    UserTable() {};
+
+  public:
+    void create(drogon::orm::DbClientPtr client);
+    void alter(drogon::orm::DbClientPtr client);
+    void _delete(drogon::orm::DbClientPtr client);
 };
 
