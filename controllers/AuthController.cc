109b4889b94902d7542c5a18223cb478f2f2d2e8
Tim <sarrtim@gmail.com>
2023-04-22 17:51:38 +0300

    Add identity ad email on payload claim & fix fields

--- controllers/AuthController.cc
+++ controllers/AuthController.cc
@@ -1,73 +1,67 @@
 #include <jwt-cpp/jwt.h>
-#include <openssl/evp.h>
-#include <openssl/bio.h>
-#include <openssl/buffer.h>
-#include <openssl/hmac.h>
 #include <sstream>
-#include <iomanip>
-#include <ctime>
 #include <drogon/HttpController.h>
 #include <drogon/orm/Mapper.h>
 #include <drogon/HttpViewData.h>
 #include <drogon/HttpResponse.h>
-#include <drogon/orm/DbClient.h>
 #include "AuthController.h"
 #include "../models/Users.h"
 
+using namespace std;
 using namespace drogon;
 using namespace drogon::orm;
 
 
-std::string generateJWT(const std::string& secretKey, const std::string& payload)
+string generateJWT(const string& secretKey, const string& email)
 {
     auto token = jwt::create()
     .set_issuer("auth0")
     .set_type("JWS")
-    .set_payload_claim("sample", jwt::claim(std::string("test")))
-    .set_issued_at(std::chrono::system_clock::now())
-    .set_expires_at(std::chrono::system_clock::now() + std::chrono::seconds{3600})
+    .set_payload_claim("identity", jwt::claim(email))
+    .set_issued_at(chrono::system_clock::now())
+    .set_expires_at(chrono::system_clock::now() + chrono::seconds{3600})
     .sign(jwt::algorithm::hs256{"secret"});
     return token;
 }
 
 void AuthController::asyncHandleHttpRequest(const HttpRequestPtr& req,
-    std::function<void(const HttpResponsePtr&)>&& callback)
+    function<void(const HttpResponsePtr&)>&& callback)
 {
-    auto email = req->getParameter("email");
-    auto password = req->getParameter("password");
-
     auto client = drogon::app().getDbClient();
-
     if (client) {
-        auto sql = "SELECT * FROM public.user WHERE email = $1";
-        auto f = client->execSqlAsyncFuture(sql, email);
-        auto result = f.get();
+        auto jsonBody = req->getJsonObject();
+        auto email = (*jsonBody)["email"].asString();
+        auto password = (*jsonBody)["password"].asString();
 
+        Mapper<drogon_model::drogon_user_service::Users> mp(client);
+        auto user = mp.findFutureBy(
+                Criteria(drogon_model::drogon_user_service::Users::Cols::_email, email));
+        auto result = user.get();
         Json::Value usersJson(Json::arrayValue);
-        for (const auto& row : result)
+        for (const auto& user : result)
         {
             Json::Value userJson;
-            userJson["id"] = row["id"].as<std::string>();
-            userJson["first_name"] = row["first_name"].as<std::string>();
-            userJson["last_name"] = row["last_name"].as<std::string>();
-            userJson["email"] = row["email"].as<std::string>();
+            userJson["id"] = user.getValueOfId();
+            userJson["first_name"] = user.getValueOfFirstName();
+            userJson["last_name"] = user.getValueOfLastName();
+            userJson["email"] = user.getValueOfEmail();
             usersJson.append(userJson);
         }
 
         // Generate token for user found here and return as json
-        const std::string secretKey = drogon::app().getCustomConfig()["secret_key"].asString();
-        std::string payload = "{\"sub\":\"1234567890\",\"name\":\"" + email +"\",\"iat\":1516239022}";
-        std::string jwt = generateJWT(secretKey, payload);
+        const string secretKey = drogon::app().getCustomConfig()["secret_key"].asString();
+        string jwt = generateJWT(secretKey, email);
 
         Json::Value response;
         response["token"] = jwt;
+        response["user"] = usersJson;
         auto resp=HttpResponse::newHttpJsonResponse(response);
         callback(resp);
 
     } else {
         Json::Value error;
         error["error"] = "Unable to connect to database";
-        auto resp=HttpResponse::newHttpJsonResponse(error);
+        auto resp = HttpResponse::newHttpJsonResponse(error);
         resp->setStatusCode(k500InternalServerError);
         callback(resp);
     }
