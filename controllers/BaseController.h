a05890d6306cb212d57af7f0b3de8d0feced124a
Tim <sarrtim@gmail.com>
2023-04-23 10:52:43 +0300

    Add handleResponse static method

--- controllers/BaseController.h
+++ controllers/BaseController.h
@@ -2,12 +2,28 @@
 
 #include <drogon/HttpSimpleController.h>
 
+#include <utility>
+
 using namespace std;
 using namespace drogon;
 
 class BaseController
 {
     public:
-        void getHeaders(const HttpRequestPtr& req, function<void(const HttpResponsePtr&)>&& callback);
-        void getByIdHeaders(const HttpRequestPtr& req, function<void(const HttpResponsePtr&)>&& callback, string id);
+        void getHeaders(
+            const HttpRequestPtr& req,
+            function<void(const HttpResponsePtr&)>&& callback);
+        void getByIdHeaders(
+            const HttpRequestPtr& req,
+            function<void(const HttpResponsePtr&)>&& callback,
+            string id);
+        static shared_ptr<HttpResponse> handleResponse(
+            const Json::Value& responseData, HttpStatusCode statusCode)
+        {
+            auto resp= HttpResponse::newHttpJsonResponse(responseData);
+            resp->setStatusCode(statusCode);
+            resp->setContentTypeCode(CT_APPLICATION_JSON);
+            resp->addHeader("Access-Control-Allow-Origin", "*");
+            return resp;
+        }
 };
