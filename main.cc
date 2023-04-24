77424975cc93d93d2329e8e7df81519e877a4ac4
Tim <sarrtim@gmail.com>
2023-04-24 10:21:54 +0300

    Format

--- main.cc
+++ main.cc
@@ -1,10 +1,12 @@
-#include <drogon/drogon.h>
 #include <drogon/HttpAppFramework.h>
 #include "controllers/UserController.h"
 #include "tables/UserTable.h"
 
+using namespace std;
+using namespace drogon;
 
-void createTables() {
+void createTables()
+{
     // User table
     UserTable userTable;
     userTable.create();
@@ -13,11 +15,11 @@ void createTables() {
 void registerRoutes()
 {
     // Register root route
-    drogon::app().registerHandler(
+    app().registerHandler(
         "/", 
         [](
-            const drogon::HttpRequestPtr& req, std::function<void (
-                const drogon::HttpResponsePtr&
+            const HttpRequestPtr& req, function<void (
+                const HttpResponsePtr&
                 )>&& callback
           ) {
         // Create tables
@@ -26,40 +28,40 @@ void registerRoutes()
         // Return response
         Json::Value response;
         response["status"] = "up";
-        drogon::HttpResponsePtr resp = drogon::HttpResponse::newHttpJsonResponse(response);
+        HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(response);
         callback(resp);
     });
 
     // Register user routes
-    auto userController = std::make_shared<UserController>();
-    drogon::app().registerHandler(
+    auto userController = make_shared<UserController>();
+    app().registerHandler(
         "/api/v1/user", 
         [userController](
         const HttpRequestPtr& req, 
-        std::function<void(const HttpResponsePtr&)>&& callback) {
+        function<void(const HttpResponsePtr&)>&& callback) {
             if (req->method() == Options) {
-                userController->getHeaders(req, std::move(callback));
+                userController->getHeaders(req, move(callback));
             } else if (req->method() == Get) {
-                userController->getUsers(req, std::move(callback));
+                userController->getUsers(req, move(callback));
             } else if (req->method() == Post) {
-                userController->createUser(req, std::move(callback));
+                userController->createUser(req, move(callback));
             }
         }, 
         {Options, Get, Post, "AuthFilter"}
     );
-    drogon::app().registerHandler(
+    app().registerHandler(
     "/api/v1/user/{id}", 
     [userController](
         const HttpRequestPtr& req, 
-        std::function<void(const HttpResponsePtr&)>&& callback, std::string id) {
+        function<void(const HttpResponsePtr&)>&& callback, const string& id) {
             if (req->method() == Options) {
-                userController->getByIdHeaders(req, std::move(callback), id);
+                userController->getByIdHeaders(req, move(callback), id);
             } else if (req->method() == Get) {
-                userController->getUserById(req, std::move(callback), id);
+                userController->getUserById(req, move(callback), id);
             } else if (req->method() == Put) {
-                userController->updateUserById(req, std::move(callback), id);;
+                userController->updateUserById(req, move(callback), id);;
             } else if (req->method() == Delete) {
-                userController->deleteUserById(req, std::move(callback), id);;
+                userController->deleteUserById(req, move(callback), id);;
             }
     }, 
     {Options, Get, Put, Delete, "AuthFilter"}
@@ -71,20 +73,20 @@ void registerRoutes()
 int main() {
 
     // Set log level
-    drogon::app().setLogLevel(trantor::Logger::kTrace);
-    std::int32_t port = 8000;
+    app().setLogLevel(trantor::Logger::kTrace);
+    int32_t port = 8000;
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", port);
+    app().addListener("0.0.0.0", port);
 
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    app().loadConfigFile("../config.json");
 
     // Register routes
     registerRoutes();
 
     // Run server
     LOG_INFO << "Server running on 127.0.0.1:" << port;
-    drogon::app().run();
+    app().run();
 
     return 0;
 }
