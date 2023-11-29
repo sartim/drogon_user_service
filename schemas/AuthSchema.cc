81e899f73ab9c97d8739515020acbdd04521d3e6
Tim <sarrtim@gmail.com>
2023-11-29 15:38:58 +0300

    Add auth schema

---
+++ schemas/AuthSchema.cc
@@ -0,0 +1,23 @@
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include "AuthSchema.h"
+
+using namespace std;
+
+std::vector<std::string> AuthSchema::validate(const std::map<std::string, std::string> &jsonBody) const
+{
+    std::vector<std::string> errors;
+
+    for (const auto &field : schema_) {
+        const std::string &fieldName = field.first;
+        bool required = field.second;
+
+        // if (required && !jsonBody.isMember(fieldName)) {
+        //     errors.push_back("Field '" + fieldName + "' is required.");
+        // }
+    }
+
+    return errors;
+};
